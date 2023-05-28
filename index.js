c6d070f56865feffe253a7d418e66e86c4b65496
r4sy4n <124577832+r4sy4n@users.noreply.github.com>
2023-05-28 21:42:40 +0800

    Feat: implemented login/register on authRoutes

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
-
+const morgan = require('morgan');
+const PORT = process.env.PORT || 8000;
+const baseURL = '/api/v1';
 //mongoose config
-mongoose.connect(``)
+mongoose.connect(``);
 
-PORT = 8000;
+//import routes
+const authRoutes = require('./routes/authRoutes');
 
 const server = express();
 server.use( bodyParser.json() );
 server.use( cors() );
 server.use( helmet() );
 //morgan
+server.use( morgan('dev') );
+
+server.use( `${baseURL}/auth`, authRoutes );
 
 server.get('/', ( request, response ) => {
     response.status(200).send(`Welcome to Express App`);
