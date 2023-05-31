259c236922452cb5f7780e906ac8532c67d58c15
r4sy4n <124577832+r4sy4n@users.noreply.github.com>
2023-05-31 21:20:09 +0800

    Feat: created logo component

---
+++ client/src/components/LogoBig.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo_big from '../assets/images/logo_big.png'
+
+const BigLogo = () => {
+  return (
+     <img src={logo_big} alt='connectify biglogo' className='logo img-logo' />
+  )
+}
+
+export default BigLogo
