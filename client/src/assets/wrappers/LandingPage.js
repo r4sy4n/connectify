9ea72758fbfb9136aab2be9be4d84edd8bd937b1
r4sy4n <124577832+r4sy4n@users.noreply.github.com>
2023-06-03 22:02:50 +0800

    Feat: created landing page

---
+++ client/src/assets/wrappers/LandingPage.js
@@ -0,0 +1,135 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+  background-color: #F3E8FF;
+
+  nav {
+    width: 90vw;
+    max-width: 1120px;
+    margin: 0 auto;
+    height: 6rem;
+    display: flex;
+    align-items: center;
+  }
+  .logo{
+    margin-bottom: 3rem;
+  }
+  .container {
+    width: 90vw;
+    max-width: 1120px;
+    width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+  }
+  .page {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100vh;
+  }
+  h1 {
+    font-weight: 700;
+    color: #EB455F;
+  }
+  h2, h3{
+    color: #2B3467;
+  }
+  p {
+    color: #041562;
+    margin: 3rem;
+  }
+  ul{
+    list-style-type: none;
+  }
+  .how-container{
+    background-color: #9DB2BF;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    height: 80vh;
+    padding-top: 6rem;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+  }
+  .why-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 6rem;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+  }
+  .brand-container{
+    background-color: #9DB2BF;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    height: 60vh;
+    padding-top: 6rem;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+  }
+  footer{
+    color: #041562;
+    background:#EB455F;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    height: 40vh;
+    padding-top: 6rem;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+  }
+  .pic{
+    width: 20rem;
+  }
+  .image-container{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 3rem;
+    padding: 3rem;
+    max-width: 100%;
+  }
+  .text{
+    background: #fff;
+    padding: 0.5rem;
+  }
+  .btn{
+    padding: 1rem 2rem;
+  }
+
+/* xs */
+/* @media (min-width: 475px) {} */
+
+/* sm */
+/* @media (min-width: 640px) {} */
+
+/* md */
+/* @media (min-width: 768px) {} */
+
+/* lg */
+/* @media (min-width: 1024px) {} */
+
+/* xl */
+/* @media (min-width: 1280px) {} */
+
+/* 2xl */
+/* @media (min-width: 1536px) {} */
+  `
+export default Wrapper;
