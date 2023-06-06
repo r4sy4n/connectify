7798a80408924eaaae23974ea0e4127343390f20
Carlo <jancarlodelmoro@gmail.com>
2023-06-06 21:56:38 +0800

    Chore: moved utilities and data inside folders

--- client/src/pages/Catalog.js
+++ client/src/pages/Catalog.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { CatalogWrapper } from '../assets/wrappers/Catalog';
-import { categories } from '../components/CategoryList';
+import { categories } from '../assets/data/CategoryList';
 
 const Catalog = () => {
   const navigate = useNavigate();
