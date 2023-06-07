2738922259ea71b13f300e16786498ed390bf7a0
loudatppl <123815534+loudatppl@users.noreply.github.com>
2023-06-07 11:44:55 +0800

    feat: contact page UI setup

---
+++ client/src/assets/wrappers/Contact.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const ContactWrapper = styled.main`
+    
+`
+
+export default ContactWrapper
--- client/src/pages/ContactUs.js
+++ client/src/pages/ContactUs.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import ContactWrapper from '../assets/wrappers/Contact'
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div>
+      <Navbar />
+      <ContactWrapper>
+        <div className='title-container'>
+          <p>Get in touch</p>
+          <h1>Contact Us</h1>
+        </div>
+        <div className='form-container'>
+          
+        </div>
+      </ContactWrapper>
+    </div>
   )
 }
 
