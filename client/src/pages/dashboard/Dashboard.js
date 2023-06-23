cc8c96ea0fda452c15acfa0274c477890cb3da36
r4sy4n <russell.ramiro@gmail.com>
2023-06-23 21:58:15 +0800

    Feat: implement dashboard data, added graphs and styling

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import Wrapper from '../../assets/wrappers/Dashboard';
+import { SharedLayoutContext } from './SharedLayout';
+import Loading from '../../components/Loading';
+import axios from 'axios';
+import { GlobalVariables } from '../../App';
+import BarChartData from '../../components/BarChartData';
+
 
 const Dashboard = () => {
+  const { showSidebar } = useContext(SharedLayoutContext);
+  const [ isLoading, setIsLoading ] = useState(true);
+    const { globalLoggedInUserId } = useContext( GlobalVariables );
+    const [orders, setorders] = useState([]);
+
+
+  useEffect(() => {
+    axios.get(`${ process.env.REACT_APP_API_BASE_URL }/api/v1/users/${ globalLoggedInUserId }/order-list`).then( response => {
+      console.log(response)
+      setorders(response.data.orderList);
+      setIsLoading(false);
+    })
+  }, [globalLoggedInUserId]);
+
+
+  if (isLoading) {
+  return <Loading center />;
+  }
+
+
   return (
-    <div>Dashboard</div>
+    <Wrapper>
+       <div className={showSidebar ? 'form' : 'form-move'}>
+        <h1>Dashboard</h1>
+        <section className='flex'>
+          <div className='grid'>
+          </div>
+        </section>
+        <section>
+          <div className='grid-chart'>
+            <BarChartData orders={orders}/>
+          </div>
+        </section>
+      </div>
+
+    </Wrapper>
   )
 }
 
-export default Dashboard
+export default Dashboard;
